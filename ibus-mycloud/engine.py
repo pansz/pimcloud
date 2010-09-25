035c08d9b3817b07c45d7a50c84e427ea1e3845a
Pan, Shi Zhu <Pan, Shi Zhu@localhost>
2010-09-25 15:05:00 +0800

    finished sentence input

--- ibus-mycloud/engine.py
+++ ibus-mycloud/engine.py
@@ -35,129 +35,172 @@ class Engine(ibus.EngineBase):
         self.__lookup_table.set_page_size(10)
         self.__prop_list = ibus.PropList()
         self.__prop_list.append(ibus.Property(u"test", icon = u"/usr/share/ibus-mycloud/icons/prop.svg"))
+        self.__state = self.state_empty
 
     def process_key_event(self, keyval, keycode, state):
         try:
-            ret = self.process_key_event_debug(keyval, keycode, state)
+            is_valid = ((state & 
+                    (modifier.RELEASE_MASK | modifier.CONTROL_MASK | modifier.ALT_MASK)) == 0)
+            if not is_valid:
+                # bypass KeyUp, Ctrl, Alt event
+                return False
+
+            # only procces KeyDown event
+            ret = self.__state(keyval, keycode, state)
         except Exception, inst:
             print type(inst).__name__, inst
             return False
         return ret
-    def process_key_event_debug(self, keyval, keycode, state):
-        # ignore key release events
-        is_press = ((state & modifier.RELEASE_MASK) == 0)
-        if not is_press:
-            return False
-        # print "%s, kv=0x%x, kc=0x%x, st=0x%x" % (self, keyval, keycode, state)
-
-        has_candidate = (self.__lookup_table.get_number_of_candidates() > 0)
-
-        if self.__preedit_string:
-            if keyval == keysyms.Return:
-                self.__commit_string(self.__preedit_string)
-            elif keyval == keysyms.Escape:
-                if has_candidate:
-                    self.__lookup_table.clean()
-                    self.__update_lookup_table()
+    def state_is(self, target):
+        return self.__state == target
+    def state_transit(self, target):
+        source = self.__state
+        self.__state = target
+        if source == self.state_empty:
+            if target == self.state_input:
+                self.__invalidate()
+        elif source == self.state_input:
+            if target == self.state_select:
+                self.__is_invalidate = False
+            elif target == self.state_empty:
                 self.__preedit_string = u""
                 self.__update()
-            elif keyval == keysyms.BackSpace:
-                if has_candidate:
-                    self.__lookup_table.clean()
-                    self.__update_lookup_table()
-                self.__preedit_string = self.__preedit_string[:-1]
+        elif source == self.state_select:
+            if target == self.state_empty:
+                self.__lookup_table.clean()
+                self.__update_lookup_table()
+                self.__update()
+            elif target == self.state_input:
                 self.__invalidate()
-            elif keyval == keysyms.space:
-                if has_candidate:
-                    self.__commit_string(self.__lookup_table.get_current_candidate().text)
-                else:
-                    preedit_len = len(self.__preedit_string)
-                    attrs = ibus.AttrList()
-                    self.__lookup_table.clean()
-                    if preedit_len > 0:
-                        res = mycloud.parsefunc(self.__preedit_string.encode("utf-8"), "172.16.55.240")
-                        if res != "":
-                            attrs.append(ibus.AttributeForeground(0xff0000, 0, preedit_len))
-                            for item in res.split("\n"):
-                                text = item.split("\t")[0]
-                                if text:
-                                    self.__lookup_table.append_candidate(ibus.Text(text))
-                    self.__update_lookup_table()
-                    self.__is_invalidate = False
-            elif keyval >= keysyms._0 and keyval <= keysyms._9:
-                if has_candidate:
-                    index = keyval - keysyms._1
-                    if index < 0:
-                        index = 10
-                    candidates = self.__lookup_table.get_candidates_in_current_page()
-                    if index < len(candidates):
-                        candidate = candidates[index].text
-                        self.__commit_string(candidate)
-                else:
-                    if state & (modifier.CONTROL_MASK | modifier.ALT_MASK) == 0:
-                        self.__preedit_string += unichr(keyval)
-                        self.__invalidate()
-            elif keyval == keysyms.minus:
-                self.page_up()
-            elif keyval == keysyms.equal:
-                self.page_down()
-            elif keyval == keysyms.Up:
-                self.cursor_up()
-            elif keyval == keysyms.Down:
-                self.cursor_down()
-            elif keyval == keysyms.Left or keyval == keysyms.Right:
-                pass
-            elif keyval in xrange(keysyms.a, keysyms.z + 1) or \
-                keyval in xrange(keysyms.A, keysyms.Z + 1):
-                if has_candidate:
-                    self.__commit_string(self.__lookup_table.get_current_candidate().text)
-                else:
-                    pass
-                if state & (modifier.CONTROL_MASK | modifier.ALT_MASK) == 0:
-                    self.__preedit_string += unichr(keyval)
-                    self.__invalidate()
-            elif keyval in xrange(keysyms.exclam, keysyms.asciitilde+1):
-                if has_candidate:
-                    self.__commit_string(self.__lookup_table.get_current_candidate().text)
-                    res = self.__query_char(keyval)
-                    if res != "":
-                        self.__commit_string(res)
-                    else:
-                        return False
-                else:
-                    if state & (modifier.CONTROL_MASK | modifier.ALT_MASK) == 0:
-                        self.__preedit_string += unichr(keyval)
-                        self.__invalidate()
-            else:
-                #print "blocked: keyval=0x%x, keycode=0x%x, state=0x%x" % (keyval, keycode, state)
-                pass
-
-            # block all input when preedit available
-            return True
         else:
-            if keyval in xrange(keysyms.a, keysyms.z + 1):
-                if state & (modifier.CONTROL_MASK | modifier.ALT_MASK) == 0:
-                    self.__preedit_string += unichr(keyval)
-                    self.__invalidate()
-                    return True
-            elif keyval in xrange(keysyms.A, keysyms.Z + 1):
-                pass
-            elif keyval >= keysyms._0 and keyval <= keysyms._9:
-                pass
-            elif keyval in xrange(keysyms.exclam, keysyms.asciitilde+1):
-                # this includes a-z, A-Z, 0-9 and all symbols
-                # since we bypassed a-zA-Z0-9, we got all symbols
-                res = self.__query_char(keyval)
-                if res != "":
-                    self.__commit_string(res)
-                    return True
-                else:
-                    pass
+            pass
+    def state_empty(self, keyval, keycode, state):
+        if keyval in xrange(keysyms.a, keysyms.z + 1):
+            self.__preedit_string = unichr(keyval)
+            self.state_transit(self.state_input)
+            return True
+        elif keyval in xrange(keysyms.A, keysyms.Z + 1):
+            pass
+        elif keyval >= keysyms._0 and keyval <= keysyms._9:
+            pass
+        elif keyval in xrange(keysyms.exclam, keysyms.asciitilde+1):
+            # this includes a-z, A-Z, 0-9 and all symbols
+            # since we bypassed a-zA-Z0-9, we got all symbols
+            res = self.__query_char(keyval)
+            if res != "":
+                self.__commit_string(res)
+                return True
             else:
-                #print "ignored: keyval=0x%x, keycode=0x%x, state=0x%x" % (keyval, keycode, state)
                 pass
+        else:
+            #print "ignored: keyval=0x%x, keycode=0x%x, state=0x%x" % (keyval, keycode, state)
+            pass
+        return False
+    def state_input(self, keyval, keycode, state):
+        if keyval == keysyms.Return:
+            self.__commit_string(self.__preedit_string)
+        elif keyval == keysyms.Escape:
+            self.state_transit(self.state_empty)
+        elif keyval == keysyms.BackSpace:
+            self.__preedit_string = self.__preedit_string[:-1]
+            self.__invalidate()
+        elif keyval == keysyms.space:
+            res = mycloud.parsefunc(self.__preedit_string.encode("utf-8"), "172.16.55.240")
+            if res != "":
+                preedit_len = len(self.__preedit_string)
+                for item in res.split("\n"):
+                    try:
+                        text, index, hint = item.split("\t")
+                        ibt = ibus.Text(text)
+                        ibt.index = int(index)
+                        ibt.hint = hint
+                        self.__lookup_table.append_candidate(ibt)
+                    except Exception:
+                        pass
+                if self.__lookup_table.get_number_of_candidates() > 0:
+                    self.__update_lookup_table()
+                    self.state_transit(self.state_select)
+            else:
+                pass
+        elif keyval == keysyms.Left or keyval == keysyms.Right:
+            pass
+        elif keyval in xrange(keysyms.exclam, keysyms.asciitilde+1):
+            # This includes all visible ascii characters: [0x21, 0x7e]
+            self.__preedit_string += unichr(keyval)
+            self.__invalidate()
+        else:
+            #print "blocked: keyval=0x%x, keycode=0x%x, state=0x%x" % (keyval, keycode, state)
+            pass
 
-            return False
+        return True
+    def sentence_update(self):
+        self.__lookup_table.clean()
+        res = mycloud.parsefunc(self.__preedit_string.encode("utf-8"), "172.16.55.240")
+        if res != "":
+            preedit_len = len(self.__preedit_string)
+            for item in res.split("\n"):
+                try:
+                    text, index, hint = item.split("\t")
+                    #self.__lookup_table.append_candidate(ibus.Text(text, (hint, index)))
+                    ibt = ibus.Text(text)
+                    ibt.index = int(index)
+                    ibt.hint = hint
+                    self.__lookup_table.append_candidate(ibt)
+                except Exception:
+                    pass
+            self.__update_lookup_table()
+            if self.__lookup_table.get_number_of_candidates() > 0:
+                self.state_transit(self.state_select)
+            else:
+                self.state_transit(self.state_input)
+            self.__is_invalidate = False
+        else:
+            pass
+    def state_select(self, keyval, keycode, state):
+        if keyval == keysyms.Return:
+            self.__commit_string(self.__preedit_string)
+        elif keyval == keysyms.Escape:
+            self.state_transit(self.state_empty)
+        elif keyval == keysyms.BackSpace:
+            self.__preedit_string = self.__preedit_string[:-1]
+            self.state_transit(self.state_input)
+        elif keyval == keysyms.space:
+            self.__commit_string(self.__lookup_table.get_current_candidate())
+            if self.state_is(self.state_select):
+                self.sentence_update()
+        elif keyval >= keysyms._0 and keyval <= keysyms._9:
+            index = keyval - keysyms._1
+            if index < 0:
+                index = 9
+            candidates = self.__lookup_table.get_candidates_in_current_page()
+            if index < len(candidates):
+                self.__commit_string(candidates[index])
+                if self.state_is(self.state_select):
+                    self.sentence_update()
+        elif keyval == keysyms.minus:
+            self.page_up()
+        elif keyval == keysyms.equal:
+            self.page_down()
+        elif keyval == keysyms.Left or keyval == keysyms.Right:
+            pass
+        elif keyval in xrange(keysyms.a, keysyms.z + 1) or \
+            keyval in xrange(keysyms.A, keysyms.Z + 1):
+            self.__commit_string(self.__lookup_table.get_current_candidate())
+            if self.state_is(self.state_empty):
+                return self.__state(keyval, keycode, state)
+            else:
+                self.sentence_update()
+        elif keyval in xrange(keysyms.exclam, keysyms.asciitilde+1):
+            self.__commit_string(self.__lookup_table.get_current_candidate())
+            if self.state_is(self.state_empty):
+                return self.__state(keyval, keycode, state)
+            else:
+                self.sentence_update()
+        else:
+            #print "blocked: keyval=0x%x, keycode=0x%x, state=0x%x" % (keyval, keycode, state)
+            pass
+
+        return True
 
     def __query_char(self, keyval):
         res = mycloud.parsefunc(chr(keyval), "172.16.55.240")
@@ -199,12 +242,18 @@ class Engine(ibus.EngineBase):
         return False
 
     def __commit_string(self, text):
-        self.commit_text(ibus.Text(text))
-        self.__preedit_string = u""
-        if self.__lookup_table.get_number_of_candidates() > 0:
-            self.__lookup_table.clean()
-            self.__update_lookup_table()
-        self.__update()
+        if isinstance(text, ibus.Text):
+            self.commit_text(text)
+            self.__preedit_string = self.__preedit_string[text.index:]
+            if self.__preedit_string == u"":
+                self.state_transit(self.state_empty)
+            else:
+                self.update_auxiliary_text(ibus.Text(self.__preedit_string), True)
+                self.state_transit(self.state_select)
+        else:
+            self.commit_text(ibus.Text(text))
+            self.__preedit_string = u""
+            self.state_transit(self.state_empty)
 
     def __update(self):
         preedit_len = len(self.__preedit_string)
@@ -217,7 +266,6 @@ class Engine(ibus.EngineBase):
     def __update_lookup_table(self):
         visible = self.__lookup_table.get_number_of_candidates() > 0
         self.update_lookup_table(self.__lookup_table, visible)
-
 
     def focus_in(self):
         self.register_properties(self.__prop_list)
